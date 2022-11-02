544953fe6a0724dea1da857437f498a078358bd1
EfrainMejiasC <efrainmejiasc@hotmail.com>
2022-11-02 07:24:35 -0400

    inicio

---
+++ DatosGTM/DatosGTMNegocio/Helpers/AdobePdfApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosGTMNegocio.Helpers
+{
+    public  class AdobePdfApi
+    {
+        public static string client_id { get; set; }
+        public static string client_secret { get; set; }
+        public static string organization_id { get; set; }
+        public static string account_id { get; set; }
+        public static string private_key_file { get; set; }
+    }
+}
