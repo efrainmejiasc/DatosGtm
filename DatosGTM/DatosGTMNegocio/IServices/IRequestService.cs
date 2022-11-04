28906bc82712b2ecf27192d61ee1436b23686faf
EfrainMejiasC <efrainmejiasc@hotmail.com>
2022-11-04 10:11:50 -0400

    Update

--- DatosGTM/DatosGTMNegocio/IServices/IRequestService.cs
+++ DatosGTM/DatosGTMNegocio/IServices/IRequestService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatosGTMNegocio.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,6 @@ namespace DatosGTMNegocio.IServices
 {
     public interface IRequestService
     {
-        Task<string> ObtenerJWTAsync(string urlAdobePdfApi);
-        Task<string> ObtenerJWTPostAsync(string urlAdobePdfApiJwt);
+        Task<AdobePdfApiTokenModel> ObtenerJWTAsync(string urlAdobePdfApi);
     }
 }
